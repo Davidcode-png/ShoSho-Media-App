c89d22d2d0b9b2d566a168757f83d548ccd310c5
DavidsonCo <opeoluwa313@gmail.com>
2022-07-20 16:48:26 +0100

    Iniital Commit

---
+++ API/Controllers/TestsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("apt/[controller]")]
+    public class TestsController: ControllerBase
+    {
+        [HttpGet]
+        public List<int> Test()
+        {
+            int[] tests = { 1, 2, 3 };
+            return tests.ToList();
+        }
+    }
+}
